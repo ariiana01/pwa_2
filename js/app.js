a3e921c8aa97771b82767fcabbc3f588d34307ea
Donikaos <104843751+Donikaos@users.noreply.github.com>
2023-11-02 16:43:43 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -62,3 +62,15 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 }
+<div class="columns">
+  <div class="column">
+    <p><button type="button" onclick="getStream('video')">Grab video</button></p>
+    
+    <video controls autoplay style="height:180px; width: 240px;"></video>
+  </div>
+  <div class="column">
+    <p><button type="button" onclick="getStream('audio')">Grab audio</button></p>
+    
+    <audio controls></audio>
+  </div>
+</div>
