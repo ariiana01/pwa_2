dd38aa1f295a35a0fef57eebb1bbbbce996210d4
Donikaos <104843751+Donikaos@users.noreply.github.com>
2023-11-02 18:12:57 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,41 +1,216 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" },
-]
+  {
+    name: "Perspiciatis",
+    image: "images/coffee1.jpg"
+  },
+
+];
 const showCoffees = () => {
-  let output = ""
+  let output = "";
   coffees.forEach(
     ({ name, image }) =>
-      (output += `
+    (output += `
               <div class="card">
                 <img class="card--avatar" src=${image} />
                 <h1 class="card--title">${name}</h1>
                 <a class="card--link" href="#">Taste</a>
               </div>
               `)
-  )
-  container.innerHTML = output
+  );
+  container.innerHTML = output;
+};
+
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
+function getUserMedia(constraints) {
+  // if Promise-based API is available, use it
+  if (navigator.mediaDevices) {
+    return navigator.mediaDevices.getUserMedia(constraints);
+  }
+
+  // otherwise try falling back to old, possibly prefixed API...
+  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+  if (legacyApi) {
+    // ...and promisify it
+    return new Promise(function (resolve, reject) {
+      legacyApi.bind(navigator)(constraints, resolve, reject);
+    });
+  }
 }
 
-document.addEventListener("DOMContentLoaded", showCoffees)
-if ('srcObject' in mediaControl) {
+function getStream(type) {
+  if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+    alert('User Media API not supported.');
+    return;
+  }
+
+  var constraints = {};
+  constraints[type] = true;
+
+  getUserMedia(constraints)
+    .then(function (stream) {
+      var mediaControl = document.querySelector(type);
+
+      if ('srcObject' in mediaControl) {
         mediaControl.srcObject = stream;
       } else if (navigator.mozGetUserMedia) {
         mediaControl.mozSrcObject = stream;
       } else {
         mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
       }
-      
+
       mediaControl.play();
     })
     .catch(function (err) {
       alert('Error: ' + err);
     });
+}
+var target = document.getElementById('target');
+var watchId;
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+  target.innerText = 'Geolocation API not supported.';
+}
+if ('DeviceOrientationEvent' in window) {
+  window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+} else {
+  document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
+}
+
+function deviceOrientationHandler(eventData) {
+  var tiltLR = eventData.gamma;
+  var tiltFB = eventData.beta;
+  var dir = eventData.alpha;
+
+  document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+  document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+  document.getElementById("doDirection").innerHTML = Math.round(dir);
+
+  var logo = document.getElementById("imgLogo");
+  logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+  logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
+  logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+}
+//--------------------------------Praktikum 3------------------------------------------------------>
+if ('localStorage' in window || 'sessionStorage' in window) {
+  var selectedEngine;
+
+  var logTarget = document.getElementById('target');
+  var valueInput = document.getElementById('value');
+
+  var reloadInputValue = function () {
+    console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
+    valueInput.value = window[selectedEngine].getItem('myKey') || '';
+  }
+
+  var selectEngine = function (engine) {
+    selectedEngine = engine;
+    reloadInputValue();
+  };
+
+  function handleChange(change) {
+    var timeBadge = new Date().toTimeString().split(' ')[0];
+    var newState = document.createElement('p');
+    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
+    logTarget.appendChild(newState);
+  }
+
+  var radios = document.querySelectorAll('#selectEngine input');
+  for (var i = 0; i < radios.length; ++i) {
+    radios[i].addEventListener('change', function () {
+      selectEngine(this.value)
+    });
+  }
+
+  selectEngine('localStorage');
+
+  valueInput.addEventListener('keyup', function () {
+    window[selectedEngine].setItem('myKey', this.value);
+  });
+
+  var onStorageChanged = function (change) {
+    var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
+    handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
+    if (engine === selectedEngine) {
+      reloadInputValue();
+    }
+  }
+
+  window.addEventListener('storage', onStorageChanged);
+  function getReadFile(reader, i) {
+    return function () {
+      var li = document.querySelector('[data-idx="' + i + '"]');
+
+      li.innerHTML += 'File starts with "' + reader.result.substr(0, 25) + '"';
+    }
+  }
+
+  function readFiles(files) {
+    document.getElementById('count').innerHTML = files.length;
+
+    var target = document.getElementById('target');
+    target.innerHTML = '';
+
+    for (var i = 0; i < files.length; ++i) {
+      var item = document.createElement('li');
+      item.setAttribute('data-idx', i);
+      var file = files[i];
+
+      var reader = new FileReader();
+      reader.addEventListener('load', getReadFile(reader, i));
+      reader.readAsText(file);
+
+
+      item.innerHTML = '' + file.name + ', ' + file.type + ', ' + file.size + ' bytes, last modified ' + file.lastModifiedDate + '';
+      target.appendChild(item);
+      window.localStorage.setItem("Kontakt", file); //wird im Cache gespeichert
+    };
+  }
+
+  async function writeFile() {
+    if (!window.chooseFileSystemEntries) {
+      alert('Native File System API not supported');
+      return;
+    }
+
+    const target = document.getElementById('target');
+    target.innerHTML = 'Opening file handle...';
+
+    const handle = await window.chooseFileSystemEntries({
+      type: 'save-file',
+    });
+
+    const file = await handle.getFile()
+    const writer = await handle.createWriter();
+    await writer.write(0, 'Hello world from What Web Can Do!');
+    await writer.close()
+
+    target.innerHTML = 'Test content written to ' + file.name + '.';
+  }
+}
