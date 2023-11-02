1f216b771012c794e59c8c613d3e2894f4148546
Donikaos <104843751+Donikaos@users.noreply.github.com>
2023-11-02 17:27:25 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,136 +1,141 @@
-const container = document.querySelector(".container");
+
+  const container = document.querySelector(".container");
 const coffees = [
   {
     name: "Perspiciatis",
-    image: "images/coffee1.jpg"
+    image: "images/coffee1.jpg",
   },
   {
     name: "Voluptatem",
-    image: "images/coffee2.jpg"
+    image: "images/coffee2.jpg",
   },
   {
     name: "Explicabo",
-    image: "images/coffee3.jpg"
+    image: "images/coffee3.jpg",
   },
   {
     name: "Rchitecto",
-    image: "images/coffee4.jpg"
+    image: "images/coffee4.jpg",
   },
   {
     name: " Beatae",
-    image: "images/coffee5.jpg"
+    image: "images/coffee5.jpg",
   },
   {
     name: " Vitae",
-    image: "images/coffee6.jpg"
+    image: "images/coffee6.jpg",
   },
   {
     name: "Inventore",
-    image: "images/coffee7.jpg"
+    image: "images/coffee7.jpg",
   },
   {
     name: "Veritatis",
-    image: "images/coffee8.jpg"
+    image: "images/coffee8.jpg",
   },
   {
     name: "Accusantium",
-    image: "images/coffee9.jpg"
-  }
+    image: "images/coffee9.jpg",
+  },
 ];
+
 const showCoffees = () => {
   let output = "";
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  );
+  coffees.forEach(({ name, image }) => {
+    output += `
+      <div class="card">
+        <img class="card--avatar" src="${image}" />
+        <h1 class="card--title">${name}</h1>
+        <a class="card--link" href="#">Taste</a>
+      </div>
+    `;
+  });
   container.innerHTML = output;
 };
 
 document.addEventListener("DOMContentLoaded", showCoffees);
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
+  window.addEventListener("load", function () {
     navigator.serviceWorker
-      .register("./serviceWorker.js", { scope: "./"})
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
+      .register("./serviceWorker.js", { scope: "./" })
+      .then((res) => console.log("Service worker registered"))
+      .catch((err) => console.log("Service worker not registered", err));
   });
 }
+
 function getUserMedia(constraints) {
-  // if Promise-based API is available, use it
   if (navigator.mediaDevices) {
     return navigator.mediaDevices.getUserMedia(constraints);
   }
-    
-  // otherwise try falling back to old, possibly prefixed API...
-  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    
+
+  var legacyApi =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
   if (legacyApi) {
-    // ...and promisify it
     return new Promise(function (resolve, reject) {
       legacyApi.bind(navigator)(constraints, resolve, reject);
     });
   }
 }
 
-function getStream (type) {
-  if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-    alert('User Media API not supported.');
+function getStream(type) {
+  if (
+    !navigator.mediaDevices &&
+    !navigator.getUserMedia &&
+    !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia &&
+    !navigator.msGetUserMedia
+  ) {
+    alert("User Media API not supported.");
     return;
   }
 
   var constraints = {};
   constraints[type] = true;
-  
+
   getUserMedia(constraints)
     .then(function (stream) {
       var mediaControl = document.querySelector(type);
-      
-      if ('srcObject' in mediaControl) {
+
+      if ("srcObject" in mediaControl) {
         mediaControl.srcObject = stream;
       } else if (navigator.mozGetUserMedia) {
         mediaControl.mozSrcObject = stream;
       } else {
         mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
       }
-      
+
       mediaControl.play();
     })
     .catch(function (err) {
-      alert('Error: ' + err);
+      alert("Error: " + err);
     });
-  function getUserMedia(options, successCallback, failureCallback) {
-  var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  if (api) {
-    return api.bind(navigator)(options, successCallback, failureCallback);
-  }
 }
 
 var theStream;
 
 function getStream() {
-  if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-    alert('User Media API not supported.');
+  if (
+    !navigator.getUserMedia &&
+    !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia &&
+    !navigator.msGetUserMedia
+  ) {
+    alert("User Media API not supported.");
     return;
   }
-  
+
   var constraints = {
-    video: true
+    video: true,
   };
 
   getUserMedia(constraints, function (stream) {
-    var mediaControl = document.querySelector('video');
-    if ('srcObject' in mediaControl) {
+    var mediaControl = document.querySelector("video");
+    if ("srcObject" in mediaControl) {
       mediaControl.srcObject = stream;
     } else if (navigator.mozGetUserMedia) {
       mediaControl.mozSrcObject = stream;
@@ -139,28 +144,29 @@ function getStream() {
     }
     theStream = stream;
   }, function (err) {
-    alert('Error: ' + err);
+    alert("Error: " + err);
   });
 }
 
 function takePhoto() {
-  if (!('ImageCapture' in window)) {
-    alert('ImageCapture is not available');
+  if (!("ImageCapture" in window)) {
+    alert("ImageCapture is not available");
     return;
   }
-  
+
   if (!theStream) {
-    alert('Grab the video stream first!');
+    alert("Grab the video stream first!");
     return;
   }
-  
+
   var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
 
   theImageCapturer.takePhoto()
-    .then(blob => {
+    .then(function (blob) {
       var theImageTag = document.getElementById("imageTag");
       theImageTag.src = URL.createObjectURL(blob);
     })
-    .catch(err => alert('Error: ' + err));
-
-  
+    .catch(function (err) {
+      alert("Error: " + err);
+    });
+}
