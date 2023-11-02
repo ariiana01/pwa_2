389e1191d8a9dffa41c5522b5bb3518c51e30151
Donikaos <104843751+Donikaos@users.noreply.github.com>
2023-11-02 17:55:51 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,123 +1,32 @@
-
-  const container = document.querySelector(".container");
+const container = document.querySelector(".container")
 const coffees = [
- 
-  container.innerHTML = output;
-};
-
-document.addEventListener("DOMContentLoaded", showCoffees);
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker
-      .register("./serviceWorker.js", { scope: "./" })
-      .then((res) => console.log("Service worker registered"))
-      .catch((err) => console.log("Service worker not registered", err));
-  });
+  { name: "Perspiciatis", image: "images/coffee1.jpg" },
+  { name: "Voluptatem", image: "images/coffee2.jpg" },
+  { name: "Explicabo", image: "images/coffee3.jpg" },
+  { name: "Rchitecto", image: "images/coffee4.jpg" },
+  { name: " Beatae", image: "images/coffee5.jpg" },
+  { name: " Vitae", image: "images/coffee6.jpg" },
+  { name: "Inventore", image: "images/coffee7.jpg" },
+  { name: "Veritatis", image: "images/coffee8.jpg" },
+  { name: "Accusantium", image: "images/coffee9.jpg" },
+]
+const showCoffees = () => {
+  let output = ""
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  )
+  container.innerHTML = output
 }
 
-function getUserMedia(constraints) {
-  if (navigator.mediaDevices) {
-    return navigator.mediaDevices.getUserMedia(constraints);
-  }
-
-  var legacyApi =
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia;
-
-  if (legacyApi) {
-    return new Promise(function (resolve, reject) {
-      legacyApi.bind(navigator)(constraints, resolve, reject);
-    });
-  }
-}
-
-function getStream(type) {
-  if (
-    !navigator.mediaDevices &&
-    !navigator.getUserMedia &&
-    !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia &&
-    !navigator.msGetUserMedia
-  ) {
-    alert("User Media API not supported.");
-    return;
-  }
-
-  var constraints = {};
-  constraints[type] = true;
-
-  getUserMedia(constraints)
-    .then(function (stream) {
-      var mediaControl = document.querySelector(type);
-
-      if ("srcObject" in mediaControl) {
-        mediaControl.srcObject = stream;
-      } else if (navigator.mozGetUserMedia) {
-        mediaControl.mozSrcObject = stream;
-      } else {
-        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-      }
-
-      mediaControl.play();
-    })
-    .catch(function (err) {
-      alert("Error: " + err);
-    });
-}
-
-var theStream;
-
-function getStream() {
-  if (
-    !navigator.getUserMedia &&
-    !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia &&
-    !navigator.msGetUserMedia
-  ) {
-    alert("User Media API not supported.");
-    return;
-  }
-
-  var constraints = {
-    video: true,
-  };
-
- function getUserMedia(constraints) {
-  // if Promise-based API is available, use it
-  if (navigator.mediaDevices) {
-    return navigator.mediaDevices.getUserMedia(constraints);
-  }
-    
-  // otherwise try falling back to old, possibly prefixed API...
-  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    
-  if (legacyApi) {
-    // ...and promisify it
-    return new Promise(function (resolve, reject) {
-      legacyApi.bind(navigator)(constraints, resolve, reject);
-    });
-  }
-}
-
-function getStream (type) {
-  if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-    alert('User Media API not supported.');
-    return;
-  }
-
-  var constraints = {};
-  constraints[type] = true;
-  
-  getUserMedia(constraints)
-    .then(function (stream) {
-      var mediaControl = document.querySelector(type);
-      
-      if ('srcObject' in mediaControl) {
+document.addEventListener("DOMContentLoaded", showCoffees)
+if ('srcObject' in mediaControl) {
         mediaControl.srcObject = stream;
       } else if (navigator.mozGetUserMedia) {
         mediaControl.mozSrcObject = stream;
@@ -130,19 +39,3 @@ function getStream (type) {
     .catch(function (err) {
       alert('Error: ' + err);
     });
-// app.js
-function getStream(type) {
-  // Implementierung der getStream-Funktion
-}
-
-function takePhoto() {
-  // Implementierung der takePhoto-Funktion
-}
-
-// Rest des JavaScript-Codes
-
-
-
-
-
-
